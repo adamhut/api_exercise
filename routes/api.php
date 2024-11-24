94bd54136297bd4ceca923057c01d31f33fef7bc
Yu Ting Huang <adamhut@gmail.com>
2024-11-24 07:57:49 -0800

    remove unused route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\Nyt\BestSellersController;
 
-//
 
 Route::group(['prefix' => '1/nyt' ], function () {
         Route::get('best-sellers', [BestSellersController::class,'index'])->name('api.v1.nyt.best-sellers.index');
